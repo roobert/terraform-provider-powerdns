87786e65bd97889a35e5a841edb90be8271f1ae8
Rob Wilson <roobert@gmail.com>
2016-01-27 12:56:58 +0000

    implement Delete method

--- powerdns/resource_a_record.go
+++ powerdns/resource_a_record.go
@@ -11,7 +11,7 @@ func resourceARecord() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceARecordCreate,
 		Read:   resourceARecordRead,
-		Update: resourceARecordUpdate,
+		Update: resourceARecordCreate,
 		Delete: resourceARecordDelete,
 
 		Schema: map[string]*schema.Schema{
@@ -52,6 +52,13 @@ type Record struct {
 }
 
 func resourceARecordCreate(d *schema.ResourceData, m interface{}) error {
+
+	// FIXME: at the moment CREATE and UPDATE are the same method because
+	//        powerdns API expects changetype to be REPLACE when creating records.
+	//
+	//        it would make sense for the CREATE method to first check if a resource
+	//        exists (using READ method?) and then call the UPDATE method.
+
 	config := m.(*Config)
 
 	name := d.Get("name").(string)
@@ -79,8 +86,10 @@ func resourceARecordCreate(d *schema.ResourceData, m interface{}) error {
 	log.Printf("PowerDNS API URL: %s\n", config.APIUrl)
 	log.Printf("PowerDNS API post data: %+v\n", data)
 
+	url := config.APIUrl
+
 	request := gorequest.New()
-	resp, body, err := request.Patch(config.APIUrl).
+	resp, body, err := request.Patch(url).
 		Set("X-API-Key", config.APIKey).
 		Send(data).
 		End()
@@ -89,12 +98,14 @@ func resourceARecordCreate(d *schema.ResourceData, m interface{}) error {
 		return fmt.Errorf("%s", err)
 	}
 
-	if resp.Status != "200 OK" {
+	if resp.StatusCode != 200 {
 		return fmt.Errorf("Error: (%s): %s", resp.Status, body)
 	}
 
 	log.Printf("PowerDNS API body: %s\n", body)
 	log.Printf("PowerDNS API response: %+v\n", resp)
+
+	d.SetId("powerdns_a_record-" + url)
 
 	return nil
 }
@@ -103,10 +114,44 @@ func resourceARecordRead(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
 
-func resourceARecordUpdate(d *schema.ResourceData, m interface{}) error {
-	return nil
-}
-
 func resourceARecordDelete(d *schema.ResourceData, m interface{}) error {
+	config := m.(*Config)
+
+	name := d.Get("name").(string)
+
+	records := []Record{}
+
+	rrset := RRSet{
+		Name:       name,
+		Changetype: "DELETE",
+		Records:    records,
+		Type:       "A",
+	}
+
+	rrsets := []RRSet{rrset}
+	data := RRSets{RRSets: rrsets}
+
+	log.Printf("PowerDNS API URL: %s\n", config.APIUrl)
+	log.Printf("PowerDNS API post data: %+v\n", data)
+
+	url := config.APIUrl
+
+	request := gorequest.New()
+	resp, body, err := request.Patch(url).
+		Set("X-API-Key", config.APIKey).
+		Send(data).
+		End()
+
+	if err != nil {
+		return fmt.Errorf("%s", err)
+	}
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("Error: (%s): %s", resp.Status, body)
+	}
+
+	log.Printf("PowerDNS API body: %s\n", body)
+	log.Printf("PowerDNS API response: %+v\n", resp)
+
 	return nil
 }
